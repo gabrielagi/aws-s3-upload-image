8dc808cdd1c27d637dd76713781ed90a8047197f
gabrielagi <gabrielagi503@gmail.com>
2023-09-23 13:11:39 -0300

    handlePhotoChange + handleTitleChange

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+  const [post, setPost] = useState({
+    title: "",
+    photo: null,
+  });
+
+  const handleTitleChange = (event) => {
+    setPost({ ...post, title: event.target.value });
+  };
+
+  const handlePhotoChange = (event) => {
+    setPost({ ...post, photo: event.target.files[0] });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div>
-      <form>
-        <input type="text" placeholder="Title" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Title"
+          value={post.title}
+          onChange={handleTitleChange}
+        />
 
-        <input type="file" name="photo" />
+        <input type="file" name="photo" onChange={handlePhotoChange} />
+
+        <button type="submit">Upload</button>
       </form>
     </div>
   );
